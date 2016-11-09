b0588e2ca8627c64655e34419c81a12989b2dae3
Augusto E Ringuelet <eze87lp@gmail.com>
2016-11-09 20:26:42 -0300

    algunas mas

---
+++ src/main/java/clasesDAO/ProfesorDAOjpa.java
@@ -0,0 +1,59 @@
+package clasesDAO;
+
+import java.util.List;
+
+import clases.Cartelera;
+import clases.Comentario;
+import clases.Profesor;
+import clases.Publicacion;
+import interfacesDAO.ProfesorDAO;
+
+public class ProfesorDAOjpa implements ProfesorDAO {
+
+	public ProfesorDAOjpa() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public List<Profesor> obtenerTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Profesor obtener(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Publicacion> obtenerPublicaciones(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Cartelera> obtenerCarteleras(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Comentario> obtenerComentarios(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void eliminar(int id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void guardar(Profesor profesor) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

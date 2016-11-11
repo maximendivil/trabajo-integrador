1f9efd9362df77c328245730aa5f1322af533b41
Augusto E Ringuelet <eze87lp@gmail.com>
2016-11-11 15:55:30 -0300

    Mas de GenericDAO

--- src/main/java/interfacesDAO/PublicacionDAO.java
+++ src/main/java/interfacesDAO/PublicacionDAO.java
@@ -8,10 +8,6 @@ import clases.Profesor;
 import clases.Publicacion;
 
 public interface PublicacionDAO {
-	List<Publicacion> obtenerTodos();
-	Publicacion obtener(int id);
 	List<Publicacion> obtenerPublicaciones(int id);
 	List<Comentario> obtenerComentarios(int id);
-	void eliminar(int id);
-	void guardar(Publicacion publicacion);
 }
